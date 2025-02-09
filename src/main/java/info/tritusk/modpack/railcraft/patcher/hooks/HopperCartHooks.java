160ed9b2a2da8c084a677d7dc1ff11618e01fefb
3TUSK <git@tritusk.info>
2025-02-09 02:57:49 -0800

    Fix build

--- src/main/java/info/tritusk/modpack/railcraft/patcher/hooks/HopperCartHooks.java
+++ src/main/java/info/tritusk/modpack/railcraft/patcher/hooks/HopperCartHooks.java
@@ -5,6 +5,8 @@ import net.minecraft.item.ItemStack;
 
 public class HopperCartHooks {
 
+    // Called from EntityCartHopper to properly update the EntityItem on the ground.
+    // Otherwise, the dropped item never decreases its count, causing dupe glitches.
     public static void handleItemRemainder(ItemStack remainder, EntityItem dropOnGround) {
         dropOnGround.setItem(remainder);
     }
