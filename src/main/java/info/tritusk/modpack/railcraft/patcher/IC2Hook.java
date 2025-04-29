8cdfd29444cd769cea27cd78823b5dbb8d0be143
3TUSK <git@tritusk.info>
2025-04-28 19:53:05 -0700

    GH-14: do not post EnergyTileUnloadedEvent if IC2 is absent

--- src/main/java/info/tritusk/modpack/railcraft/patcher/IC2Hook.java
+++ src/main/java/info/tritusk/modpack/railcraft/patcher/IC2Hook.java
@@ -1,9 +1,12 @@
 package info.tritusk.modpack.railcraft.patcher;
 
+import ic2.api.energy.EnergyNet;
+import ic2.api.energy.event.EnergyTileUnloadEvent;
 import mods.railcraft.common.blocks.logic.IC2EmitterLogic;
 import mods.railcraft.common.plugins.ic2.IC2Plugin;
 import mods.railcraft.common.util.misc.Game;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
 
 public class IC2Hook {
 
@@ -30,9 +33,14 @@ public class IC2Hook {
 
     public static void removeFromENet0(IC2EmitterLogic logic) {
         try {
-            IC2Plugin.removeTileFromNet(logic);
+            // GH-14: always check the presence of EnergyNet.instance before dispatching event.
+            // This ensures we won't get NPE because of uninitialized API.
+            // Unfortunately, the IC2 API is always present because Railcraft repacks IC2 API.
+            if (EnergyNet.instance != null) {
+                MinecraftForge.EVENT_BUS.post(new EnergyTileUnloadEvent(logic));
+            }
         } catch (Throwable error) {
-            Game.log().api("IndustrialCraft", error);
+            Game.log().api("IC2", error, EnergyTileUnloadEvent.class);
         }
     }
 }
